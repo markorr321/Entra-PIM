833c55e014782f591ed0c83dba4cb00a6e1c1d73
Mark Orr <markorr321@gmail.com>
2026-02-02 13:34:30 -0600

    Clean Up

--- Check-Cache.ps1
+++
@@ -1,8 +0,0 @@
-$cache = Join-Path ([System.IO.Path]::GetTempPath()) 'EntraPIM_UpdateCheck.json'
-if (Test-Path $cache) {
-    Write-Host "Cache file found at: $cache" -ForegroundColor Green
-    Write-Host ""
-    Get-Content $cache | ConvertFrom-Json | Format-List
-} else {
-    Write-Host "Cache file not found" -ForegroundColor Red
-}
